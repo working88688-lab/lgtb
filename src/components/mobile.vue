da25f3616ce69f066c118c9834b7c65d18fba110
heitu <heitu@gmail.com>
2026-03-26 12:56:55 +0800

    H5+PC 落地页

--- src/components/mobile.vue
+++ src/components/mobile.vue
@@ -3,54 +3,10 @@
 		<div v-if="initData.isWeixin" class="weixinbg">
 			<img :src="weixinbg" alt="" />
 		</div>
-		<header>
-			<img src="../assets/mobile/logo.png" alt="" class="logo" />
-			<div class="groups">
-				<img
-					src="../assets/mobile/tg.png"
-					alt=""
-					class="group"
-					@click.stop="addGroup(1)"
-				/>
-				<img
-					src="../assets/mobile/hz.png"
-					alt=""
-					class="group"
-					@click.stop="addGroup(2)"
-				/>
-			</div>
-		</header>
-		<swiper ref="mySwiper" class="h5Swiper" :options="swiperOptions">
-			<swiper-slide>
-				<div class="page">
-					<img src="../assets/mobile/p1.png" alt="" class="pic pic1" />
-					<!-- <img src="../assets/mobile/ll.png" alt="" class="text" /> -->
-				</div>
-			</swiper-slide>
-			<swiper-slide>
-				<div class="page">
-					<img src="../assets/mobile/p2.png" alt="" class="pic pic2" />
-					<!-- <img src="../assets/mobile/ll.png" alt="" class="text" /> -->
-				</div>
-			</swiper-slide>
-		</swiper>
-		<!-- <img
-      v-if="initData.sys == 'ios'"
-      src="../assets/mobile/tips2.png"
-      alt=""
-      class="tips"
-      @click.stop="toTutorial"
-    />
-    <img
-      v-else
-      src="../assets/mobile/tips1.png"
-      alt=""
-      class="tips"
-      @click.stop="toTutorial"
-    /> -->
+
+		<!-- 移动端更简单：上方仅展示一张图，下方是 footer -->
+		<img src="../assets/mobile/mobile.png" alt="" class="mobile-top-img" />
 		<footer>
-			<img src="../assets/mobile/text.png" alt="" class="text" />
-			<img src="../assets/mobile/pic.png" alt="" class="pic" />
 			<div class="btns">
 				<img src="../assets/mobile/ios.png" alt="" class="btn" />
 				<img src="../assets/mobile/an1.png" alt="" class="btn" />
@@ -83,73 +39,22 @@ export default {
 		},
 	},
 	data() {
-		let self = this;
 		return {
 			weixinbg,
 			copyStr: "",
-			codeValue: "",
-			initPage: false,
-			timer: null,
-			showDownload: false,
-			maskShow: false,
-			vid: undefined,
-			gifIndex: 0,
-			frames: [],
-			headerIndex: 0,
-			footerIndex: 0,
-			bounceFlag: 0,
-			swiperOptions: {
-				speed: 1000,
-				notNextTick: true,
-				autoplay: {
-					delay: 3000,
-					disableOnInteraction: false,
-				},
-				loop: true,
-				loopAdditionalSlides: 3,
-				direction: "horizontal",
-				setWrapperSize: true,
-				// autoHeight: true,horizontal
-				slidesPerView: 1,
-				mousewheel: true,
-				mousewheelControl: true,
-				resistanceRatio: 0,
-				observeParents: true,
-				on: {
-					init: function () {
-						self.pageIndex = this.activeIndex;
-					},
-					slideChangeTransitionStart: function () {
-						self.pageIndex = this.activeIndex;
-					},
-				},
-			},
 		};
 	},
 	created() {
-		this.codeValue = window.location.href;
 		this.copyStr = localStorage.getItem("copyStr");
-		const copyData = JSON.parse(this.copyStr);
-		this.vid = Number(copyData.vid);
 		if (this.initData.sys == "android") {
 			this.getChApkData();
 		}
 	},
 	mounted() {
-		this.initPage = true;
 		const clipboard = new Clipboard(".copy");
 		clipboard.on("success", () => {
 			console.log("复制成功");
 		});
-		// if (this.initData.sys == "android") {
-		//   this.showDownload = true
-		// }
-		this.showGif();
-	},
-	computed: {
-		swiper() {
-			return this.$refs.mySwiper.$swiper;
-		},
 	},
 	methods: {
 		getChApkData() {
@@ -176,48 +81,6 @@ export default {
 			}
 			// 统一点击埋点上报
 			this.reportEvent(this.initData.sys, e);
-			// this.$router.push({ path: "/tutorial" })
-			// setTimeout(() => {
-			// }, 500)
-		},
-		showGif() {
-			clearInterval(this.timer);
-			this.timer = setInterval(() => {
-				if (this.gifIndex < this.frames.length - 1) {
-					this.gifIndex++;
-				} else {
-					this.gifIndex = 0;
-					// clearInterval(this.timer)
-				}
-			}, 33.33);
-		},
-		setHeader(i) {
-			this.headerIndex = i;
-			this.bounce();
-		},
-		setFooter(i) {
-			this.footerIndex = i;
-			this.bounce();
-		},
-		bounce() {
-			if ("vibrate" in navigator) {
-				navigator.vibrate(300);
-			}
-			this.bounceFlag = 1;
-			setTimeout(() => {
-				this.bounceFlag = 0;
-			}, 600);
-		},
-		addGroup(type) {
-			// type == 1
-			//   ? this.jump("https://t.me/mmbushinese")
-			//   : this.jump("https://t.me/kwai1tv");
-			type == 1
-				? this.jump(this.initData.customerData.link2)
-				: this.jump(this.initData.customerData.link);
-		},
-		toTutorial() {
-			this.$router.push({ path: "/tutorial" });
 		},
 	},
 };
@@ -232,79 +95,57 @@ export default {
 	right: 0;
 	background: rgba(0, 0, 0, 0.5);
 	z-index: 99999;
+
 	img {
 		width: 100%;
 	}
 }
+
 .mobile {
 	width: 100vw;
 	height: 100vh;
 	position: relative;
-	background: url("../assets/mobile/bg.jpg");
-	background-size: 100% 100%;
-	header {
+
+	/* 上方图片 + 下方 footer 两段式布局 */
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+
+	.mobile-top-img {
 		width: 100%;
-		padding: calc(14vw / 7.5) calc(16vw / 7.5);
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		position: absolute;
-		top: 0;
-		z-index: 100;
-		.logo {
-			width: calc(358vw / 7.5);
-		}
-		.groups {
-			display: flex;
-			align-items: center;
-			.group {
-				width: calc(65vw / 7.5);
-				margin-left: calc(62vw / 7.5);
-			}
-		}
+		flex: 1 1 auto;
+		min-height: 0;
+		height: 100%;
+		object-fit: fill;
+		/* 与背景图铺满一致，避免裁切 */
+		display: block;
 	}
+
 	footer {
 		width: 100vw;
-		position: fixed;
-		top: unset;
-		bottom: 0;
+		position: relative;
+		bottom: auto;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		background-color: #6d61cf;
 		z-index: 100;
-		.text {
-			width: calc(579vw / 7.5);
-		}
-		.pic {
-			width: calc(706vw / 7.5);
-			margin-bottom: calc(32vw / 7.5);
-		}
+		flex-shrink: 0;
+		padding: 12px 10px 10px;
+
 		.btns {
 			width: 100%;
 			display: flex;
 			justify-content: space-around;
-			margin-bottom: calc(32vw / 7.5);
+			margin-bottom: calc(6/760 * 100vh);
+
 			.btn {
-				width: calc(215vw / 7.5);
+				width: calc(110 / 360 * 100vw);
 			}
 		}
+
 		.tips {
-			width: calc(469vw / 7.5);
-			margin-bottom: calc(28vw / 7.5);
-		}
-	}
-	.h5Swiper {
-		width: 100vw;
-		height: 100vh;
-		.page {
-			width: 100vw;
-			height: 100vh;
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			.pic {
-				width: 100%;
-			}
+			width: calc(277 / 360 * 100vw);
 		}
 	}
 }
--- src/components/pc.vue
+++ src/components/pc.vue
@@ -1,37 +1,39 @@
 <template>
 	<div class="pc">
-		<header>
-			<img src="../assets/pc/logo.png" alt="" class="logo" />
-			<div class="groups">
-				<img
-					src="../assets/pc/tg.png"
-					alt=""
-					class="group"
-					@click="addGroup(1)"
-				/>
-				<img src="../assets/pc/hz.png" class="group" @click="addGroup(2)" />
-			</div>
-		</header>
-		<div class="acts">
-			<img src="../assets/pc/text.png" alt="" class="text" />
-			<div class="c">
+		<!-- 用 img 定位背景图（不要 background），铺满舞台 -->
+		<img src="../assets/pc/pc.png" class="pc-bg-img" alt="" />
+
+		<!-- 右上角按钮：UI 1920x1080，按钮 384x138，上边 32px，右边距 60px，间距 16px -->
+		<!-- 左按钮（商务）：btn-sw -->
+		<img
+			src="../assets/pc/btn-sw.png"
+			class="pc-btn pc-btn-sw"
+			alt=""
+			@click="addGroup(2)"
+		/>
+		<!-- 右按钮（TG群）：btn-tg -->
+		<img
+			src="../assets/pc/btn-tg.png"
+			class="pc-btn pc-btn-tg"
+			alt=""
+			@click="addGroup(1)"
+		/>
+
+		<!-- 左侧二维码卡片（右半部分展示 quark/tips 两张图） -->
+		<div class="qrCard" @click="downApp($event)">
+			<div class="qrWrap">
 				<qrcode-vue
-					:key="pageIndex"
+					:key="codeValue"
 					:value="codeValue"
 					class="code"
 					:title="codeValue"
 				/>
 			</div>
+			<div class="qrText">
+				<img src="../assets/pc/quark.png" class="quark" alt="" />
+				<img src="../assets/pc/tips.png" class="tipsImg" alt="" />
+			</div>
 		</div>
-		<img src="../assets/pc/pic.png" alt="" class="pic" />
-		<swiper ref="mySwiper" class="pcSwiper" :options="swiperOptions">
-			<swiper-slide>
-				<div class="page page1"></div>
-			</swiper-slide>
-			<swiper-slide>
-				<div class="page page2"></div>
-			</swiper-slide>
-		</swiper>
 	</div>
 </template>
 
@@ -40,7 +42,7 @@ import QrcodeVue from "qrcode.vue";
 import actions from "../utils/actions";
 
 export default {
-	name: "Mobile",
+	name: "Pc",
 	mixins: [actions],
 	components: { QrcodeVue },
 	props: {
@@ -52,86 +54,19 @@ export default {
 		},
 	},
 	data() {
-		let self = this;
 		return {
-			copyStr: "",
 			codeValue: "",
-			initPage: false,
-			timer: null,
-			showCode: false,
-			vid: undefined,
-			gifIndex: 0,
-			frames,
-			headerIndex: 0,
-			footerIndex: 0,
-			pageIndex: 0,
-			bounceFlag: 0,
-			scrollSpeed: 2,
-			distance: 0,
-			swiperOptions: {
-				speed: 1000,
-				notNextTick: true,
-				autoplay: {
-					delay: 3000,
-					disableOnInteraction: false,
-				},
-				loop: true,
-				loopAdditionalSlides: 3,
-				direction: "horizontal",
-				setWrapperSize: true,
-				// autoHeight: true,horizontal,vertical
-				slidesPerView: 1,
-				mousewheel: true,
-				mousewheelControl: true,
-				resistanceRatio: 0,
-				observeParents: true,
-				on: {
-					init: function () {
-						self.pageIndex = this.activeIndex;
-					},
-					slideChangeTransitionStart: function () {
-						self.pageIndex = this.activeIndex;
-					},
-				},
-			},
 		};
 	},
 	created() {
 		this.codeValue = window.location.href;
-		this.copyStr = localStorage.getItem("copyStr");
-		const copyData = JSON.parse(this.copyStr);
-		this.vid = Number(copyData.vid);
 	},
-	mounted() {
-		this.initPage = true;
-	},
-	computed: {},
 	methods: {
 		downApp(e) {
-			// PC端显示二维码，触发点击埋点
+			// PC端二维码区块点击埋点
 			this.reportEvent(this.initData.sys, e);
 		},
-		setHeader(i) {
-			this.headerIndex = i;
-			this.bounce();
-		},
-		setFooter(i) {
-			this.footerIndex = i;
-			this.bounce();
-		},
-		bounce() {
-			if ("vibrate" in navigator) {
-				navigator.vibrate(300);
-			}
-			this.bounceFlag = 1;
-			setTimeout(() => {
-				this.bounceFlag = 0;
-			}, 600);
-		},
 		addGroup(type) {
-			// type == 1
-			//   ? this.jump("https://t.me/mmbushinese")
-			//   : this.jump("https://t.me/kwai1tv");
 			type == 1
 				? this.jump(this.initData.customerData.link2)
 				: this.jump(this.initData.customerData.link);
@@ -142,128 +77,114 @@ export default {
 
 <style lang="scss" scoped>
 .pc {
-	width: 100%;
-	height: 100%;
+	width: 100vw;
+	height: 100vh;
 	position: relative;
-	header {
-		width: 100%;
-		padding: 0 calc(110vh / 10.8);
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
+	overflow: hidden;
+
+	.qrCard {
 		position: absolute;
-		top: calc(46vh / 10.8);
-		z-index: 99;
-		.logo {
-			width: calc(456vh / 10.8);
-		}
-		.groups {
+		left: calc(208 / 1920 * 100vw);
+		top: calc(780 / 1080 * 100vh);
+		bottom: auto;
+		z-index: 20;
+		width: calc(493 / 1920 * 100vw);
+		height: calc(199 / 1080 * 100vh);
+		background-color: #fff;
+		border-radius: calc(12 / 1920 * 100vw);
+		padding: calc(12 / 1920 * 100vw);
+		box-sizing: border-box;
+
+		display: flex;
+		align-items: center;
+		overflow: hidden;
+		cursor: pointer;
+		gap: calc(16 / 1920 * 100vw);
+
+		.qrWrap { 
+			width: calc(171 / 1920 * 100vw);
+			height: calc(171 / 1080 * 100vh);
+			background: #fff;
 			display: flex;
+			justify-content: center;
 			align-items: center;
-			.group {
-				width: calc(65vh / 10.8);
-				margin-left: calc(62vh / 10.8);
-			}
-		}
-	}
-	.acts {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		z-index: 10;
-		position: absolute;
-		bottom: 0;
-		left: calc(78vw / 19.2);
-		.text {
-			width: calc(681vh / 10.8);
-			margin-bottom: calc(70vh / 10.8);
-		}
-		.c {
-			width: calc(703vh / 10.8);
-			height: calc(348vh / 10.8);
-			background: url("../assets/pc/cbg.png");
-			background-size: 100% 100%;
-			position: relative;
+			flex: 0 0 auto;
+
 			.code {
-				width: calc(250vh / 10.8);
-				height: calc(250vh / 10.8);
-				background: #fff;
+				width: calc(171 / 1920 * 100vw);
+				height: calc(171 / 1080 * 100vh);
 				display: flex;
 				justify-content: center;
 				align-items: center;
-				position: absolute;
-				bottom: calc(28vh / 10.8);
-				right: calc(61vh / 10.8);
-				::v-deep canvas {
-					width: calc(240vh / 10.8) !important;
-					height: calc(240vh / 10.8) !important;
-				}
+			}
+
+			::v-deep canvas {
+				width: 100% !important;
+				height: 100% !important;
 			}
 		}
-	}
-	.pic {
-		width: calc(900vh / 10.8);
-		position: absolute;
-		bottom: calc(7vh / 10.8);
-		right: calc(11vh / 10.8);
-		z-index: 10;
-	}
-	.pcSwiper {
-		.page {
-			width: 100vw;
-			height: 100vh;
+
+		.qrText {
+			flex: 1;
+			width: auto;
+			margin-left: 0;
+			height: 100%;
+
 			display: flex;
+			flex-direction: column;
 			justify-content: center;
-			align-items: flex-end;
+			align-items: flex-start; /* 右侧两张图宽度不同，左对齐不拉伸 */
 			overflow: hidden;
 
-			.container {
-				margin-bottom: calc(80vh / 10.8);
-				.text {
-					width: calc(942vh / 10.8);
-				}
-				.codebox {
-					display: flex;
-					align-items: center;
-					opacity: 0;
-					.code {
-						width: calc(225vh / 10.8);
-						height: calc(225vh / 10.8);
-						background: #fff;
-						display: flex;
-						justify-content: center;
-						align-items: center;
-						::v-deep canvas {
-							width: calc(222vh / 10.8) !important;
-							height: calc(222vh / 10.8) !important;
-						}
-					}
-					.tips {
-						width: calc(504vh / 10.8);
-						margin-left: calc(20vh / 10.8);
-					}
-				}
-			}
-			.pic {
-				position: relative;
-				width: calc(766vh / 10.8);
-			}
-			.pic.bottom {
-				bottom: -43vh;
+			img {
+				height: auto;
+				display: block;
+				width: auto;
 			}
 
-			.pic.pic2 {
-				width: calc(766vh / 15);
+			.quark {
+				width: calc(196 / 1920 * 100vw);
+			}
+
+			.tipsImg {
+				width: calc(283 / 1920 * 100vw);
+				margin-top: calc(20 / 1080 * 100vh);
 			}
 		}
-		.page1 {
-			background: url("../assets/pc/bg1.jpg");
-			background-size: 100% 100%;
-		}
-		.page2 {
-			background: url("../assets/pc/bg2.jpg");
-			background-size: 100% 100%;
-		}
 	}
+}
+
+.pc-bg-img {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	object-fit: fill;
+	pointer-events: none;
+	z-index: 0;
+}
+
+.pc-btn {
+	position: absolute;
+	top: calc(32 / 1080 * 100vh);
+	// 按钮原始尺寸 384x138：只用 width 控制，height 自动保持比例
+	width: calc(128 / 1920 * 100vw);
+	height: auto;
+	display: block;
+	cursor: pointer;
+	user-select: none;
+	z-index: 99;
+}
+
+/* TG（右边按钮）：右边距 60px */
+.pc-btn-tg {
+	right: calc(60 / 1920 * 100vw);
+}
+
+/* 商务（左边按钮）：右边距 60 + 384 + 16 = 460px */
+.pc-btn-sw {
+	// 右边距：60 + 384 + 16 = 460
+	right: calc(220 / 1920 * 100vw);
 }
 </style>
